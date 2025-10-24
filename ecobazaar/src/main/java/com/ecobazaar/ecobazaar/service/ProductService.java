c3231944594c95b74d93e70e76716f8cc5d1afeb
NEXT GEN <NEXT GEN@DESKTOP-C9V74D4>
2025-10-24 19:49:33 +0530

    QR code part2 . Download QR code

--- ecobazaar/src/main/java/com/ecobazaar/ecobazaar/service/ProductService.java
+++ ecobazaar/src/main/java/com/ecobazaar/ecobazaar/service/ProductService.java
@@ -1,5 +1,9 @@
 package com.ecobazaar.ecobazaar.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional; // ✅ use Spring’s version
@@ -8,7 +12,8 @@ import com.ecobazaar.ecobazaar.repository.ProductRepository;
 import com.ecobazaar.ecobazaar.repository.UserRepository;
 import com.ecobazaar.ecobazaar.model.User;
 import java.time.LocalDate;
-
+import com.google.zxing.WriterException;
+import com.ecobazaar.ecobazaar.util.QrCodeGenerator;
 
 @Service
 public class ProductService {
@@ -44,4 +49,57 @@ public class ProductService {
     public List<Product> filterProduct(String cropName, LocalDate endDate){
     	return productRepository.filterProducts(cropName, endDate);
     }
+    
+    public String generateProductQr(Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new RuntimeException("Product Not Found"));
+
+        String qrText = "https://farmchainx.com/products/" + product.getId();
+
+        
+        String qrPath = "uploads/qrcodes/product_" + productId + ".png";
+
+        try {
+            
+            File qrFile = new File(qrPath);
+            File parentDir = qrFile.getParentFile();
+            if (parentDir != null && !parentDir.exists()) {
+                boolean created = parentDir.mkdirs();
+                if (!created) {
+                    throw new RuntimeException("Failed to create directory: " + parentDir.getAbsolutePath());
+                }
+            }
+
+            
+            QrCodeGenerator.generateQR(qrText, qrPath);
+
+           
+            product.setQrCodePath(qrPath);
+            productRepository.save(product);
+
+            return qrPath;
+        } catch (WriterException | IOException e) {
+            throw new RuntimeException("Error generating QR: " + e.getMessage());
+        }
+    }
+    
+    public byte[] getProductQRImage(Long productId) {
+    	Product product = productRepository.findById(productId)
+    			.orElseThrow(()-> new RuntimeException("Product not found"));
+    	
+    	String qrPath = product.getQrCodePath();
+    	
+    	if(qrPath == null||qrPath.isEmpty()) {
+    		throw new RuntimeException("QR code is not generated yet for this product");
+    	}
+    	
+    	try {
+    		Path path = Path.of(qrPath);
+    		return Files.readAllBytes(path);
+    	}catch(IOException e) {
+    		throw new RuntimeException("Unable to read the QR code image :"+e.getMessage());
+    	}
+    	
+    }
+
 }
